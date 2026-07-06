018cc10952ec72f3d59902072d16746a14d86233
reda1d <mohamedseddar20@gmail.com>
2026-07-06 23:23:25 +0100

    az1

--- app.js
+++ app.js
@@ -386,6 +386,7 @@ async function submitCheckout(e) {
             // تم تعديل الترويسة (Headers) هنا لترسل كـ application/json الحقيقية
             const response = await fetch("https://script.google.com/macros/s/AKfycbwQaeUU6mAhwUEmF2NA1gm0961KMtDrstKpzwYOWCxBWkXvWg-Td1_2nmQ0kKXaNlIPGw/exec", {
                 method: "POST",
+                mode: "no-cors", //
                 headers: {
                     "Content-Type": "text/plain;charset=utf-8"
                 },
